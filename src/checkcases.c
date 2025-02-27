0d199e51a28264725a821f06e7a1407886e1bd48
quochieng <aiyanarich64@gmail.com>
2025-02-27 17:38:11 +0300

    feat: create function 'printvalue' which is responsible in printing name and password in terminal

--- src/checkcases.c
+++ src/checkcases.c
@@ -1,18 +1,30 @@
 #include "header.h"
 
-void checkCases(int choice) {
+void checkCases(sqlite3 *db ,int choice) {
+    char name[100]; // Allocate memory for name
+    char password[100]; // Allocate memory for password
+ 
+
     switch(choice) {
         case 1:
             // Login functionality
             break;
         case 2:
+            PrintValue(name, password);
+            AddTableContent(db, name, password);
             // Register functionality
             break;
         case 3:
             printf("Exiting program...\n");
-            exit(0); // This will exit the program
-            break;
+            exit(0); // Exit the program
         default:
             printf("Invalid option. Please try again.\n");
     }
+}
+
+void PrintValue(char *name, char *password) {
+    printf("Enter your name: ");
+    scanf("%99s", name); // Use %99s to avoid buffer overflow
+    printf("Enter your password: ");
+    scanf("%99s", password);
 }
