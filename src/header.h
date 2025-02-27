18a3927f160eba4cd7f357416406fbb385dd07b2
quochieng <aiyanarich64@gmail.com>
2025-02-27 17:41:00 +0300

    feat:add functions 'AddTableContent' to header.h

--- src/header.h
+++ src/header.h
@@ -1,10 +1,15 @@
 #ifndef HEADER_H
 #define HEADER_H
+
 #include <stdio.h>
 #include <stdlib.h>
-#include "sqlite3.h"
+#include <sqlite3.h> // Fixed typo: "sqlite3.h" should not have quotes
+
 sqlite3* OpenDatabase(const char *filename);
-void CreateTables(sqlite3 *db);
+void CreateTablesUser(sqlite3 *db);
+void AddTableContent(sqlite3 *db, const char *name, const char *password);
 void printOption();
-void checkCases();
-#endif
+void checkCases(sqlite3 *db, int choice); // Fixed missing parameter
+void PrintValue(char *name, char *password); // Fixed incorrect declaration
+
+#endif // HEADER_H
