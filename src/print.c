42fffa88a337eb456a085959ae261d7d80dc0343
quochieng <aiyanarich64@gmail.com>
2025-02-28 07:50:38 +0300

    feat:Add function that is used to print options on screen, and also the one to clear screen before any option is dis[layed 'cleraScreen' clears screen before an option is displayed -'printbankoption' displays several options that user can select from concerning bank details

--- src/print.c
+++ src/print.c
@@ -1,10 +1,36 @@
 #include "header.h"
 
 void printOption(){
+    clearScreen();
     printf("=======ATM=======\n\n");
     printf("-->>feel free to login/register\n\n");
     printf("[1]- login\n\n");
     printf("[2]- register\n\n");
     printf("[3]- exit\n\n");
+
+}
+
+
+void PrintBankOptions(){
+    clearScreen();
+    printf("=======ATM=======\n\n");
+    printf("-->>feel free to choose one of the option below <<--\n\n");
+    printf("[1]- Create a new account\n\n");
+    printf("[2]- Update Information of account\n\n");
+    printf("[3]- Check accounts\n\n");
+    printf("[4]- Check list of owned account\n\n");
+    printf("[5]- Make Transaction\n\n");
+    printf("[6]- Remove Existing\n\n");
+    printf("[7]- Transfer Ownership\n\n");
+    printf("[3]- exit\n\n");
+
+}
+
+void clearScreen(){
+    #ifdef _WIN32
+    system("cls");
+    #else
+    system("clear");
+    #endif
 
 }
