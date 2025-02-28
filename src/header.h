696d8190ef8ce4df3d6f7f29f0a8d2241c7f5ab7
quochieng <aiyanarich64@gmail.com>
2025-02-28 07:47:27 +0300

    feat: just add Register, Print_login,Print register in where headers

--- src/header.h
+++ src/header.h
@@ -4,12 +4,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sqlite3.h> // Fixed typo: "sqlite3.h" should not have quotes
-
+#include <string.h>  // ✅ Include this for strcmp()
+void clearScreen();
 sqlite3* OpenDatabase(const char *filename);
 void CreateTablesUser(sqlite3 *db);
-void AddTableContent(sqlite3 *db, const char *name, const char *password);
+void Register(sqlite3 *db, const char *name, const char *password);
+void Login(sqlite3 *db , const char *name, const char *password);
+void Print_Register(char *name,char *password);
+void Print_Login(char *name, char *password);
 void printOption();
+void PrintBankOptions();
 void checkCases(sqlite3 *db, int choice); // Fixed missing parameter
-void PrintValue(char *name, char *password); // Fixed incorrect declaration
+
 
 #endif // HEADER_H
