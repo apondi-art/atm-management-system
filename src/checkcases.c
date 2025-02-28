d819eaeaadc3c8e373c5e01711e046ee8d93afc6
quochieng <aiyanarich64@gmail.com>
2025-02-28 07:45:17 +0300

    feat: add function that ensures a user can register, log in and also exit as well function 'print_login' Display optionn thatt can be used to capture user log detains -function 'login' after the capturing of user login detains , it checks collected data then match with the one at the database -'Print_Register' display information that can be uses to capture user name and password -'Register' inserts user details into database

--- src/checkcases.c
+++ src/checkcases.c
@@ -1,30 +1,53 @@
 #include "header.h"
 
-void checkCases(sqlite3 *db ,int choice) {
-    char name[100]; // Allocate memory for name
+void checkCases(sqlite3 *db, int choice)
+{
+    char name[100];     // Allocate memory for name
     char password[100]; // Allocate memory for password
- 
 
-    switch(choice) {
-        case 1:
-            // Login functionality
-            break;
-        case 2:
-            PrintValue(name, password);
-            AddTableContent(db, name, password);
-            // Register functionality
-            break;
-        case 3:
-            printf("Exiting program...\n");
-            exit(0); // Exit the program
-        default:
-            printf("Invalid option. Please try again.\n");
+    switch (choice)
+    {
+    case 1:
+        // Login functionality
+     
+        Print_Login(name, password);
+        Login(db, name, password);
+        break;
+    case 2:
+    
+       Print_Register(name,password);
+        Register(db, name, password);
+
+        // Register functionality
+        break;
+    case 3:
+        printf("Exiting program...\n");
+        exit(0); // Exit the program
+    default:
+        printf("Invalid option. Please try again.\n");
     }
 }
 
-void PrintValue(char *name, char *password) {
-    printf("Enter your name: ");
-    scanf("%99s", name); // Use %99s to avoid buffer overflow
-    printf("Enter your password: ");
+void Print_Register(char *name, char *password){
+    clearScreen();
+    printf("Bank Management system\n");
+    printf("Enter Your Name :");
+    scanf("%99s", name);
+    printf("\n\n");
+    printf("Enter the password to Register:  ");
     scanf("%99s", password);
+    printf("\n\n");
+
 }
+
+void Print_Login(char *name, char *password){
+    clearScreen();
+    printf("Bank Management system\n");
+    printf("User Login :");
+    scanf("%99s", name);
+    printf("\n\n\n");
+    printf("Enter the password to login:  ");
+    scanf("%99s", password);
+
+}
+
