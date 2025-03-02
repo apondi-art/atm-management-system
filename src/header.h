04390e39e43dd0e8c4408f5e1c00874ad4065de8
quochieng <aiyanarich64@gmail.com>
2025-03-03 02:28:33 +0300

    -add function createtable,Register,handle error incase there is unsuccessfull login,BankRecords,CreateAccount,CheckCases,CaseBankOption,List All users Account,UpdatePhone,Update,UpdateCountry all to my header fil

--- src/header.h
+++ src/header.h
@@ -7,14 +7,22 @@
 #include <string.h>  // ✅ Include this for strcmp()
 void clearScreen();
 sqlite3* OpenDatabase(const char *filename);
-void CreateTablesUser(sqlite3 *db);
+void CreateTables(sqlite3 *db);
 void Register(sqlite3 *db, const char *name, const char *password);
-void Login(sqlite3 *db , const char *name, const char *password);
+int Login(sqlite3 *db , const char *name, const char *password);
 void Print_Register(char *name,char *password);
 void Print_Login(char *name, char *password);
+int handleLoginError();
 void printOption();
 void PrintBankOptions();
-void checkCases(sqlite3 *db, int choice); // Fixed missing parameter
+void BankRecords(char *Account_Number, char *Account_Type, char *Country, char *Phone, double *Balance, char *Creation_date);
+int CreateAccount(sqlite3 *db, int user_ID, const char *Account_Number, const char *Account_Type,  char *Country,  char *Phone, double Balance, const char *Creation_date);
+int checkCases(sqlite3 *db, int choice); 
+void CaseBankOption(sqlite3 *db , int choice,int User_Id);
+void List_All_UserAccount(sqlite3 *db, int User_Id);
+void Update_Phone(sqlite3 *db, char *Phone, const char *Account_number);
+void Update(sqlite3 *db);
+void Update_Country(sqlite3 *db, char *country ,const char *Account_number);
 
 
 #endif // HEADER_H
