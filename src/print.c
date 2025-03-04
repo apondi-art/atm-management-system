d05b9caec299aa6970404be891798c8d00951e03
quochieng <aiyanarich64@gmail.com>
2025-03-04 18:01:52 +0300

    create function input_centered for printing text on terminal

--- src/print.c
+++ src/print.c
@@ -1,12 +1,17 @@
 #include "header.h"
+#define TERMINAL_WIDTH 80 
 
 void printOption(){
     clearScreen();
-    printf("=======ATM=======\n\n");
-    printf("-->>feel free to login/register\n\n");
-    printf("[1]- login\n\n");
-    printf("[2]- register\n\n");
-    printf("[3]- exit\n\n");
+    printf("\n\n");
+    print_centered("=======ATM=======");
+    printf("\n\n");
+    print_centered("-->> Feel free to login/register");
+    printf("\n\n");
+    print_centered(" 1 - Login    ");  //
+    print_centered(" 2 - Register ");
+    print_centered(" 3 - Exit     ");  // 
+    printf("\n\n");
 
 }
 
@@ -15,16 +20,20 @@ void printOption(){
 
 void PrintBankOptions(){
     clearScreen();
-    printf("=======ATM=======\n\n");
-    printf("-->>feel free to choose one of the option below <<--\n\n");
-    printf("[1]- Create a new account\n\n");
-    printf("[2]- Update Information of account\n\n");
-    printf("[3]- Check accounts\n\n");
-    printf("[4]- Check list of owned account\n\n");
-    printf("[5]- Make Transaction\n\n");
-    printf("[6]- Remove Existing\n\n");
-    printf("[7]- Transfer Ownership\n\n");
-    printf("[8]- exit\n\n");
+    printf("\n\n");
+    print_centered("=======ATM=======");
+    printf("\n\n");
+    print_centered("-->>feel free to choose one of the option below <<--");
+    printf("\n\n");
+    print_centered("  [1]- Create a new account         ");
+    print_centered("  [2]- Update Information of account");
+    print_centered("  [3]- Check accounts               ");
+    print_centered("  [4]- Check list of owned account  ");
+    print_centered("  [5]- Make Transaction             ");
+    print_centered("  [6]- Remove Existing              ");
+    print_centered("  [7]- Transfer Ownership           ");
+    print_centered("  [8]- exit                         ");
+    printf("\n\n");
 
 }
 
@@ -37,3 +46,14 @@ void clearScreen(){
 
 }
 
+
+void print_centered(const char *text){
+   int text_length = strlen(text);
+    int padding = (TERMINAL_WIDTH - text_length) / 2;
+
+    for (int i = 0; i < padding; i++) {
+        printf(" ");
+    }
+    printf("%s\n", text);
+
+}
